cf5189660a3e469cfded955bab691fc20c7f43d6
aidi.yang <eddiedi@126.com>
2020-06-26 18:35:15 +0800

    更新代码文件

--- admin/src/main.ts
+++ admin/src/main.ts
@@ -2,12 +2,14 @@ import Vue from 'vue'
 import axios from 'axios'
 import App from './App.vue'
 import './plugins/element'
+import './plugins/avue'
+
 import router from './router'
-import EleForm from 'vue-ele-form'
 
 
-// 注册 vue-ele-form
-Vue.use(EleForm)
+// import EleForm from 'vue-ele-form'
+// // 注册 vue-ele-form
+// Vue.use(EleForm)
 
 Vue.config.productionTip = false
 
--- admin/src/router/index.ts
+++ admin/src/router/index.ts
@@ -2,8 +2,10 @@ import Vue from 'vue'
 import VueRouter, { RouteConfig } from 'vue-router'
 import Home from '../views/Home.vue'
 import Main from '../views/Main.vue'
-import CourseList from '../views/courses/CourseList.vue'
-import CourseEdit from '../views/courses/CourseEdit.vue'
+// import CourseList from '../views/courses/CourseList.vue'
+// import CourseEdit from '../views/courses/CourseEdit.vue'
+import ResourceCrud from '../views/ResourceCrud.vue'
+
 
 Vue.use(VueRouter)
 
@@ -13,9 +15,10 @@ Vue.use(VueRouter)
     component: Main,
     children:[
       {name:'home',path:'/',component:Home},
-      {name:'courses-list',path:'/courses/list',component:CourseList},
-      {name:'courses-eidt',path:'/courses/edit/:id',component:CourseEdit,props:true},
-      {name:'courses-create',path:'/courses/create',component:CourseEdit},
+      {name:'courses-crud',path:'/:resource/list',component:ResourceCrud,props:true},
+      // {name:'courses-list',path:'/courses/list',component:CourseList},
+      // {name:'courses-eidt',path:'/courses/edit/:id',component:CourseEdit,props:true},
+      // {name:'courses-create',path:'/courses/create',component:CourseEdit},
       
     ]
   }
